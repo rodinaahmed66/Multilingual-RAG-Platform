f9d65ae81318bc641765502249d584c62dbaec06
Rodina Ahmed <109543826+rodinaahmed66@users.noreply.github.com>
2026-06-09 20:25:03 +0000

    change

--- models/ProjectModel.py
+++ models/ProjectModel.py
@@ -31,7 +31,7 @@ class ProjectModel(BaseDataModel):
     async def create_project(self,project:Project):
         result=await self.collection.insert_one(project.dict(by_alias=True,exclude_unset=True))
         project.id=result.inserted_id
-        return result.inserted_id
+        return project
       
     async def get_project_or_create_one(self,project_id: str):
         record=await self.collection.find_one(
